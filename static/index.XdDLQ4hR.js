152be72b279ede51e1d3c0a72ae76bfbbaf11b23
Garry <>
2024-02-06 00:24:18 +0800

    fix

---
+++ static/index.XdDLQ4hR.js
@@ -0,0 +1,1 @@
+import{t as l,N as m}from"./entry.H9kDu8K8.js";function f(e,a){const{title:r,titleTemplate:n,...o}=e;return l({title:r,titleTemplate:n,_flatMeta:o},{...a,transform(t){const s=m({...t._flatMeta});return delete t._flatMeta,{...t,meta:s}}})}export{f as u};
---
+++ static/processDataIndex.y80syfaZ.js
@@ -0,0 +1,7 @@
+import{T as s,U as r,V as o,F as _}from"./entry.H9kDu8K8.js";const d=s(async()=>{let t,e;const a=([t,e]=r(()=>o(()=>import("./list.s3V2ejG1.js"),__vite__mapDeps([]),import.meta.url)),t=await t,e(),t);_("datas",()=>JSON.stringify(a))});export{d as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
