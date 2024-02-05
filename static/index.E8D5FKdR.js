86b3188439269b062d4b90b7bb194dd04db8eee8
Garry <>
2024-02-06 01:04:47 +0800

    fix

--- static/index.E8D5FKdR.js
+++
@@ -1,1 +0,0 @@
-import{u as s}from"./vue.f36acd1f.28CRYk9p.js";import{M as u}from"./entry.UoJ35UKu.js";function M(e,a){const{title:r,titleTemplate:o,...m}=e;return s({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const n=u({...t._flatMeta});return delete t._flatMeta,{...t,meta:n}}})}export{M as u};
---
+++ static/index.eorQKWwA.js
@@ -0,0 +1,1 @@
+import{u as s}from"./vue.f36acd1f.ARpsO-xi.js";import{M as u}from"./entry.bK-TAwko.js";function M(e,a){const{title:r,titleTemplate:o,...m}=e;return s({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const n=u({...t._flatMeta});return delete t._flatMeta,{...t,meta:n}}})}export{M as u};
