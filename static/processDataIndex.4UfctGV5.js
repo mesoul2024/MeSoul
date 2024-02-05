842e954d8542e6d7127da1a493511c0c9e0996d2
Garry <>
2024-02-06 00:53:02 +0800

    fix

---
+++ static/processDataIndex.4UfctGV5.js
@@ -0,0 +1,7 @@
+import{Z as s,_ as r,$ as o,E as _}from"./entry.UoJ35UKu.js";const d=s(async()=>{let t,e;const a=([t,e]=r(()=>o(()=>import("./list.s3V2ejG1.js"),__vite__mapDeps([]),import.meta.url)),t=await t,e(),t);_("datas",()=>JSON.stringify(a))});export{d as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
