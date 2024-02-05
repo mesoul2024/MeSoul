917a82d8bba0945f087fb149e4e3936ff6939c9b
Garry <>
2024-02-06 00:41:30 +0800

    turn off ssr

--- static/processDataIndex.rfbGJCQI.js
+++
@@ -1,7 +0,0 @@
-import{T as s,U as r,V as o,F as _}from"./entry.sUbKRgAS.js";const d=s(async()=>{let t,e;const a=([t,e]=r(()=>o(()=>import("./list.s3V2ejG1.js"),__vite__mapDeps([]),import.meta.url)),t=await t,e(),t);_("datas",()=>JSON.stringify(a))});export{d as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
