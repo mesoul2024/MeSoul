2a0946941052e0643b380f5c1cba06af481bcd6f
Garry <>
2024-02-07 21:14:10 +0800

    fix

---
+++ static/processDataIndex.g_KsZuVD.js
@@ -0,0 +1,7 @@
+import{_ as s,$ as r,a0 as o,H as _}from"./entry.-S7cXBBB.js";const d=s(async()=>{let t,e;const a=([t,e]=r(()=>o(()=>import("./list.tvySesJP.js"),__vite__mapDeps([]),import.meta.url)),t=await t,e(),t);_("datas",()=>JSON.stringify(a))});export{d as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
