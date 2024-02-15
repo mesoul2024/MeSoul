fa89f9e9e1caa4573feb6bdd867188935e2fe4e7
Garry <>
2024-02-15 17:39:48 +0800

    feat: ad

---
+++ static/processDataIndex.B_ZF4s2U.js
@@ -0,0 +1,7 @@
+import{a3 as s,a4 as r,a5 as o,H as _}from"./entry.D34VFS4F.js";const d=s(async()=>{let t,e;const a=([t,e]=r(()=>o(()=>import("./list.-7VxlFad.js"),__vite__mapDeps([]),import.meta.url)),t=await t,e(),t);_("datas",()=>JSON.stringify(a))});export{d as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
--- static/processDataIndex.CROj90qk.js
+++
@@ -1,7 +0,0 @@
-import{_ as s,$ as r,a0 as o,H as _}from"./entry.CKnu4Czd.js";const d=s(async()=>{let t,e;const a=([t,e]=r(()=>o(()=>import("./list.-7VxlFad.js"),__vite__mapDeps([]),import.meta.url)),t=await t,e(),t);_("datas",()=>JSON.stringify(a))});export{d as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
